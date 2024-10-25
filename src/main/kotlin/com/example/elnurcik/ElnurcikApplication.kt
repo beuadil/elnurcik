ae2911740b1d5ab16947e43555c0747cee411450
beuadil <aqasimov3@std.beu.edu.az>
2024-10-25 10:09:32 +0400

    elnr

---
+++ src/main/kotlin/com/example/elnurcik/ElnurcikApplication.kt
@@ -0,0 +1,11 @@
+package com.example.elnurcik
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElnurcikApplication
+
+fun main(args: Array<String>) {
+    runApplication<ElnurcikApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/elnurcik/ElnurcikApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.elnurcik
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ElnurcikApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
